99adf6e3a17bd8365f7076f27373995e91986ca5
elor504 <74613768+elor504@users.noreply.github.com>
2025-06-12 17:15:51 +0300

    Organized the names of the homework

---
+++ Assets/Homework/Scripts/GameManagerHW.cs
@@ -0,0 +1,14 @@
+using Fusion;
+using UnityEngine;
+namespace Homework01
+{
+    public class GameManagerHW : NetworkBehaviour
+    {
+
+        public override void Spawned()
+        {
+            base.Spawned();
+
+        }
+    }
+}
