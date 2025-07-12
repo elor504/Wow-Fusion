3166bd3a733ae111edc1d0552db94b01fb56924e
elor504 <74613768+elor504@users.noreply.github.com>
2025-07-12 22:24:06 +0300

    Added jump for more animation

--- Assets/Homework/Scripts/Character/CharacterInputHW.cs
+++ Assets/Homework/Scripts/Character/CharacterInputHW.cs
@@ -9,16 +9,22 @@ public class CharacterInputHW : NetworkBehaviour
     [SerializeField] private Animator animator;
     [Header("Movement Settings")]
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float jumpHeight;
+
     [Header("Shooting Settings")]
     [SerializeField] private float shootingCD;
 
     private bool _initialized;
-
+    private float _gravity = -9.81f;
+    private bool _isGrounded;
+    private float _yVelocity;
     private Vector2 _movementInput;
     private Vector3 _movement;
-
+    private Vector3 _rotation;
+    private Vector3 _finalMovement;
     private float _shootCounter;
-
+    private NetworkRunner _myRunner;
 
     public event Action<PlayerRef> ShootAction;
 
@@ -29,9 +35,11 @@ public class CharacterInputHW : NetworkBehaviour
         if (Object.HasStateAuthority)
         {
             GameManagerHW.Instance.PlayerCamera.SetCameraOnObject(transform);
+            _rotation = transform.localRotation.eulerAngles;
             //For some reason if i try to move my character even after Spawned() being called it resets its position
             //So i added Delay
             StartCoroutine(DelayedInitialization());
+            _myRunner = GameTest.GetMyRunner();
         }
     }
     private IEnumerator DelayedInitialization()
@@ -44,15 +52,40 @@ public class CharacterInputHW : NetworkBehaviour
     public override void FixedUpdateNetwork()
     {
         if (!_initialized) return;
+        _isGrounded = characterController.isGrounded;
 
-        _movementInput.x = Input.GetAxis("Horizontal");
-        _movementInput.y = Input.GetAxis("Vertical");
-        if (_movementInput.magnitude > float.Epsilon)
+        _movementInput.x = Input.GetAxisRaw("Horizontal");
+        _movementInput.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.E))
         {
-            _movement.x = _movementInput.x * movementSpeed * Time.fixedDeltaTime;
-            _movement.z = _movementInput.y * movementSpeed * Time.fixedDeltaTime;
-            characterController.Move(_movement);
+            _rotation.y += rotationSpeed * _myRunner.DeltaTime;
+            transform.localRotation = Quaternion.Euler(_rotation);
         }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            _rotation.y -= rotationSpeed * _myRunner.DeltaTime;
+            transform.localRotation = Quaternion.Euler(_rotation);
+        }
+
+        if (_isGrounded && _yVelocity < 0)
+        {
+            _yVelocity = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _yVelocity = Mathf.Sqrt(jumpHeight * -2f * _gravity);
+            Debug.Log($"Jump: {_yVelocity}");
+        }
+
+        _movement = (transform.forward * _movementInput.y) + (transform.right * _movementInput.x);
+        _finalMovement = _movement + (transform.up * _yVelocity);
+        characterController.Move(_finalMovement * _myRunner.DeltaTime);
+
+
+
+
         if (_shootCounter > 0)
         {
             //https://doc.photonengine.com/fusion/v1/tutorials/host-mode-basics/3-prediction
@@ -68,7 +101,9 @@ public class CharacterInputHW : NetworkBehaviour
         animator.SetFloat("MovementX", _movementInput.x);
         animator.SetFloat("MovementY", _movementInput.y);
         animator.SetBool("Walk", _movementInput.magnitude > float.Epsilon);
-
+        animator.SetBool("Jump", _yVelocity > float.Epsilon);
+        animator.SetBool("Idle", _movementInput.magnitude < float.Epsilon && _yVelocity == 0);
+        _yVelocity += _gravity * _myRunner.DeltaTime;
     }
 
 }
