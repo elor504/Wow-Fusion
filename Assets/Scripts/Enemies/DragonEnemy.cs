2e19791b916101e572b4220450ce8cf94c0cc9f9
elor504 <74613768+elor504@users.noreply.github.com>
2025-08-07 20:54:25 +0300

    Added Dragon (testing for scene object for now)

---
+++ Assets/Scripts/Enemies/DragonEnemy.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragonEnemy : MonoBehaviour, ITargetableEntity, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("References")]
+    [SerializeField] private EnemyHealth health;
+
+
+    [Header("Targetable")]
+    [SerializeField] private GameObject hoveringVisual;
+    [SerializeField] private GameObject beingTargetedVisual;
+
+    [Header("Visual Transform")]
+    [SerializeField] private Transform hitPosition;
+    [SerializeField] private Transform mouthPosition;
+
+    public bool CanBeTargeted()
+    {
+        return false;
+    }
+
+    public bool CanCastSpell(int amount)
+    {
+        return true;
+    }
+
+    public void DealDamage(ITargetableEntity caster, int damage)
+    {
+        health.DealDamage(damage);
+    }
+
+    public GameObject GetEntityGO()
+    {
+        return gameObject;
+    }
+
+    public int GetHealth()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Transform GetHitPosition()
+    {
+        return hitPosition;
+    }
+
+    public int GetMana()
+    {
+        return 0;
+    }
+
+    public Transform GetProjectileSpawnPosition()
+    {
+        return mouthPosition;
+    }
+
+    public void Heal(ITargetableEntity caster)
+    {
+
+    }
+
+    public void OnHovering()
+    {
+        hoveringVisual.SetActive(true);
+    }
+    public void OnStoppedHovering()
+    {
+        hoveringVisual.SetActive(false);
+    }
+
+    public void OnStopTargeting()
+    {
+        hoveringVisual.SetActive(false);
+    }
+
+    public void OnTargeted()
+    {
+        hoveringVisual.SetActive(true);
+    }
+
+    public bool IsAlly()
+    {
+        return false;
+    }
+    public bool IsEnemy()
+    {
+        return true;
+    }
+
+    public bool TryGetEntityStat(out EntityStat entityStat)
+    {
+        entityStat = null;
+        return false;
+    }
+    public bool TryGetEntityVisualPosition(out CharacterVFXVisual vfxVisual)
+    {
+        vfxVisual = null;
+        return false;
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TargetManager.SetCurrentHoveredEntity(this);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TargetManager.SetCurrentHoveredEntity(null);
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,32 @@
+using Fusion;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    private int maxHealth;
+    [Networked] public int CurrentHP { get; private set; }
+
+    public bool IsDead => CurrentHP <= 0;
+
+    public void Init(int maxHP)
+    {
+        maxHealth = maxHP;
+        CurrentHP = maxHealth;
+    }
+
+    [Rpc(RpcSources.All,RpcTargets.StateAuthority)]
+    public void DealDamage(int damage,RpcInfo source = default)
+    {
+        //TODO: Add the damage dealth by the player to this enemy into some kind of a recorder so show who dealth the most damage in the dungeon
+
+        CurrentHP -= damage;
+        if (CurrentHP < 0)
+        {
+            CurrentHP = 0;
+            //Death
+        }
+
+    }
+
+}
