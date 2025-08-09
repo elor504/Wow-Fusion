b4abfcbaa60f171a57553d1b82495a2b16ca87d7
elor504 <74613768+elor504@users.noreply.github.com>
2025-08-09 12:37:20 +0300

    Update BaseProjectile.cs

--- Assets/Scripts/Projectiles/BaseProjectile.cs
+++ Assets/Scripts/Projectiles/BaseProjectile.cs
@@ -34,7 +34,7 @@ public class BaseProjectile : MonoBehaviour
         transform.position += dir * (_speed * Time.deltaTime);
         
         _distance = Vector3.Distance(transform.position, _target.position);
-        if (_distance <= 0.1f)
+        if (_distance <= 0.2f)
         {
             _isActive = false;
             var vfx = VFXPoolSystem.Instance.GetAvailableObjectFromPool(hitVfxPF, transform.position);
